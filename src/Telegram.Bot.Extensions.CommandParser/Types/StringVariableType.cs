c667eb8f32420e20c6e0b10f9b0680d1b7a99061
Aleksey Usatov <usatov.aleksey@gmail.com>
2020-08-11 15:52:32 +0300

    Allow any non-whitespace char in string parser

--- src/Telegram.Bot.Extensions.CommandParser/Types/StringVariableType.cs
+++ src/Telegram.Bot.Extensions.CommandParser/Types/StringVariableType.cs
@@ -2,7 +2,7 @@ namespace Telegram.Bot.Extensions.CommandParser.Types
 {
     public class StringVariableType : IVariableType
     {
-        public string Pattern => "[a-zA-Z0-9_-]+";
+        public string Pattern => "\\S+";
         public string Name => "string";
     }
 }
