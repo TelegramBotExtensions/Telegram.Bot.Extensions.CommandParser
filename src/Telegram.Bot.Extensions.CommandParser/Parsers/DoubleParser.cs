bc8c66bf07601e8706b23217d321477da4f1551c
Aleksey Usatov <usatov.aleksey@gmail.com>
2020-08-11 15:51:41 +0300

    Fix double parser

--- src/Telegram.Bot.Extensions.CommandParser/Parsers/DoubleParser.cs
+++ src/Telegram.Bot.Extensions.CommandParser/Parsers/DoubleParser.cs
@@ -1,13 +1,17 @@
 using System;
+using System.Globalization;
 
 namespace Telegram.Bot.Extensions.CommandParser.Parsers
 {
     public class DoubleParser : IArgumentParser
     {
+        private static readonly NumberStyles Style = NumberStyles.AllowDecimalPoint |
+                                                     NumberStyles.AllowLeadingSign;
+
         public Type Type => typeof(double);
         public object? Parse(string value, string? format = default)
         {
-            if (double.TryParse(value, out var result))
+            if (double.TryParse(value, Style, CultureInfo.InvariantCulture, out var result))
             {
                 return result;
             }
